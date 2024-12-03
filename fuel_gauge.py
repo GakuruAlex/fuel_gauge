59d34cb694cf08f64e2cef5f5b41b9245dcb3489
Alex Gakuru <gakurualexgicimo@gmail.com>
2024-12-03 12:51:33 +0300

    gauge_to_fuel method and fuel_output method

--- fuel_gauge.py
+++ fuel_gauge.py
@@ -1,13 +1,65 @@
 class FuelGauge:
-    def gauge_to_fuel(self, fuel: str) -> str:
+    def gauge_to_fuel(self, fuel: str) -> int:
         """_Convert the gauge display to percentage of fule remaining_
 
         Args:
             fuel (str): _A fraction of fule as displayed on the gauge_
 
         Returns:
-            str: _Percentage of fuel remaining_
+            int: _Percentage of fuel remaining_
         Example:
             >>> gauge_to_fuel("1/4")
-            "25%" 
+            25
+            >>> gauge_to_fuel("1/2")
+            50
         """
+        
+        num, denom = fuel.split("/")
+        try:
+            if int(num) > int(denom):
+                print("Numerator greater than denominator. Try again!")
+                return self.main()
+        except ValueError:
+            print(f"One or both inputs not integers! Try again.")
+            return self.main()
+        else:
+            try:
+                percentage = round(int(num) / int(denom) * 100)
+            except ZeroDivisionError:
+                print(f"Erro! Division by zero")
+                return self.main()
+            else:
+                return percentage
+    def fuel_output(self, fuel: int) -> str:
+        """_Convert fuel to str to output on the screen_
+
+        Args:
+            fuel (int): _percentage of remaining fuel_
+
+        Returns:
+            str: _Str to output
+        Example:
+            >>> fuel_output(25)
+                "25%"
+            >>> fuel_output(1)
+                "E"
+            >>> fuel_output(100)
+                "F"
+        """
+        if fuel <= 1:
+            return "E"
+        elif fuel >= 99:
+            return "F"
+        else:
+            return f"{fuel}%"
+    
+    def main(self):
+        gauge = input("Fraction: ")
+        percentage = self.gauge_to_fuel(gauge)
+        output = self.fuel_output(percentage)
+        print(f"{output}")
+
+if __name__ == "__main__":
+    fuelgauge = FuelGauge()
+    fuelgauge.main()
+            
